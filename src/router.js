5761b66ba709f511a0801c142c9070f53b9d4ba4
WuXiaohong <wxh16144@qq.com>
2018-09-13 15:30:18 +0800

    fex:修改路由按需加载

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const router = new VueRouter({
     {
       path: "/",
       name:'index',
-      component:  (res) => require(['@/views/index'],res)
+      component:  ()=> import('@/views/index')
     }
   ]
 })
