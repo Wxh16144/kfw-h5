1fe00bda3e9e82448e3e7de5d786c122e4e984ba
WuXiaohong <wxh16144@qq.com>
2018-09-13 15:19:51 +0800

    feat:添加目录store

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: WuXiaohong 
+ * @Date: 2018-09-13 14:29:03 
+ * @Last Modified by: WuXiaohong_notebook
+ * @Last Modified time: 2018-09-13 14:56:47
+ */
+import Vue from 'vue';
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title:'Hello',
+    msg:'world'
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
