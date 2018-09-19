75a73b861f27b7b036a7b7c724b3ad98ecf9b6ca
WuXiaohong <wxh16144@qq.com>
2018-09-19 18:26:04 +0800

    修改逻辑代码

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
  * @Author: WuXiaohong 
  * @Date: 2018-09-13 14:29:03 
  * @Last Modified by: WuXiaohong_notebook
- * @Last Modified time: 2018-09-19 16:37:21
+ * @Last Modified time: 2018-09-19 18:24:38
  */
 import Vue from 'vue';
 import Vuex from 'vuex'
@@ -12,44 +12,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    activityCode: 10086,
+    activityCode: 1525850762556,
     activityStatus: true // 活动状态 默认可以用
   },
   mutations: {
-    myAlert(msg) {
-      this.$toast({
-        message: msg,
-        iconClass: 'iconfont icon-error',
-        duration: 1000
-      })
-    }
+
   },
   actions: {
     // 获取活动状态
     async checkActivity(store, self) {
+      const myAlert = msg => {
+        self.$toast({
+          message: msg,
+          iconClass: 'iconfont icon-error',
+          duration: 1000
+        })
+      }
       try {
         const response = await self.$http.post('getCache', {
-          code: 1537155092345
+          code: store.state.activityCode
         })
-        const {
-          status,
-          param: {
-            startdate,
-            enddate
-          }
-        } = response
+        const {status ,  param = {startdate:0,enddate:1} } = response
+        const { startdate  , enddate} = param
         const Datenow = Date.now().toString().substr(0, 10) - 0
-        if (status) {
+        if (status && param) {
           if (Datenow > enddate) {
             store.activityStatus = false
-            this.myAlert('活动已经结束')
+            myAlert('活动已经结束')
           } else if (Datenow < startdate) {
             store.activityStatus = false
-            this.myAlert('活动还未开始')
+            myAlert('活动还未开始')
+          }else{
+            myAlert('请检查')
           }
         } else {
           store.activityStatus = false
-          this.myAlert('服务器繁忙')
+          myAlert('服务器繁忙')
         }
       } catch (e) {
         console.error(e)
