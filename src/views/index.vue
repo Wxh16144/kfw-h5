12c437360dfb4a97d40f6985b9b18bad836efd79
WuXiaohong <wxh16144@qq.com>
2018-09-19 14:44:28 +0800

    修改index.vue的swiper初始化

--- src/views/index.vue
+++ src/views/index.vue
@@ -14,6 +14,7 @@
 
 <script>
 import Swiper from 'swiper' // 在app.vue引入
+/* eslint-disable*/
 import { mapState, mapGetters, mapActions } from 'vuex'
 export default {
   name: 'index',
@@ -30,11 +31,11 @@ export default {
   },
 
   mounted () {
+    /* eslint-disable*/
     new Swiper('.swiper-container', {
       direction: 'vertical', // 垂直
       mousewheel: true, // 开启鼠标滚轮控制Swiper切换。可设置鼠标选项，或true使用默认值。
-      grabCursor: true, // 设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状。（根据浏览器形状有所不同）
-      // effect: "fade", //slide的切换效果，默认为"slide"（位移切换），可设置为'slide'（普通切换、默认）,"fade"（淡入）"cube"（方块）"coverflow"（3d流）"flip"（3d翻转）。
+      grabCursor: true, // 设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状。（根据浏览器形状有所
       on: {
         init: function () {
           swiperAnimateCache(this) // 隐藏动画元素
@@ -44,16 +45,6 @@ export default {
           swiperAnimate(this) // 每个slide切换结束时也运行当前slide动画
         }
       }
-      // autoHeight: true, //高度随内容变化
-      // height:667
-      // loop: true,
-      // // 如果需要分页器
-      // pagination: ".swiper-pagination",
-      // // 如果需要前进后退按钮
-      // nextButton: ".swiper-button-next",
-      // prevButton: ".swiper-button-prev",
-      // // 如果需要滚动条
-      // scrollbar: ".swiper-scrollbar"
     })
   },
 
