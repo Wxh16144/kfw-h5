26dcd492665f133339f0f70004403e5ee8daff4c
WuXiaohong <wxh16144@qq.com>
2018-09-27 15:05:17 +0800

    修改打包配置

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/2018/wxhtest/',
 
     /**
      * Source Maps
