073c30e414a5dae098c604a22b269e255423cee7
WuXiaohong <wxh16144@qq.com>
2018-10-23 18:47:18 +0800

    添加微信网页授权

---
+++ src/components/token.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="token">
+    <!-- 用户授权页面 -->
+    <p>授权中...</p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    const {token, name = 'index'} = this.$route.params
+    return {
+      token,
+      name
+    }
+  },
+  mounted () {
+    // 存入 auth
+    window.localStorage.setItem('auth', this.token)
+    // this.$store.commit('updateAuth', this.token)
+    setTimeout(() => {
+      this.$router.replace({
+        name: this.name
+      })
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.token{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p{
+    font-family: myfont;
+    color: #735328;
+    font-size: 1.2rem;
+    text-align: center;
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,24 +1,54 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-
-
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import QS from 'query-string'
 // compnents
 // import index from '@/views/index'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [{
-    path: "/",
-    name: 'index',
-    component: () =>
-      import('@/views/index')
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('@/views/index')
+    },
+    {
+      path: '/token/:name/:token',
+      name: 'token',
+      component: () => import('@/components/token')
+    },
+    {
+      path: '*',
+      redirect: {
+        name: 'index'
+      }
+    }
+  ]
 })
-
 
 router.beforeEach((to, from, next) => {
-  next()
+  const _time = window.localStorage.getItem('token_timestamp') || 0
+  if (
+    (window.localStorage.getItem('auth') === null && to.name !== 'token') ||
+    Date.now() - _time > 3600000
+  ) {
+    const baseURL = 'http://api.mp.kfw001.com/auth/wechat/web'
+    const { name } = to
+    const { protocol, host, pathname } = window.location
+    console.warn(protocol, host, pathname)
+    const urlParams = QS.stringify({
+      response_type: 'auth',
+      appid: 'test',
+      scope: 'snsapi_base', // snsapi_userinfo
+      // 在地址后面添加时间戳,防止后端重定向不刷新页面,微信缓存机制  Date.now()
+      redirect_uri: `${protocol}//${host}${pathname}?${Date.now()}#/token/${name}/{auth}`
+    })
+    window.localStorage.setItem('token_timestamp', new Date().getTime())
+    window.location.href = `${baseURL}?${urlParams}`
+  } else {
+    next()
+  }
 })
 
-export default router;
+export default router
