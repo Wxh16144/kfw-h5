0b6f87352be6e42f8c6b2de1e53de3d41d31d429
WuXiaohong <wxh16144@qq.com>
2018-11-08 12:21:26 +0800

    新增箭头图标

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <music></music>
-    <phone></phone>
+    <!-- 音乐组件 -->
+    <music />
+    <!-- 手机旋转提示 -->
+    <phone />
+    <!-- 箭头组件 -->
+    <arrows />
+
     <h3>活动状态: {{ activityStatus }} </h3>
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
@@ -12,7 +17,7 @@
 <script>
 import music from '@/components/music.vue'
 import phone from '@/components/phone.vue'
-
+import arrows from '@/components/arrow.vue'
 // 微信api
 /* eslint-disable*/
 import wxConfig from '@/plugins/wxConfig.js'
@@ -27,7 +32,7 @@ export default {
     }
   },
   components: {
-    music, phone
+    music, phone ,arrows
   },
   computed: {
     ...mapState({
@@ -48,7 +53,7 @@ export default {
 // 动画css
 @import url('./assets/css/animate.min.css');
 body{
-  background-color: #eee;
+  background-color: #989898;
 }
 * {
   margin: 0;
@@ -56,21 +61,5 @@ body{
 }
 #app {
   background-color: #eee;
-}
-// 如果需要添加自定义字体
-/*完美解决css3 web-safe字体各种浏览器兼容问题*/
-@font-face {
-  font-family: 'myfont'; /*定义字体名称*/
-  src: url('./assets/font/fzylhjt.eot'); /*兼容eot*/
-  src: url('./assets/font/fzylhjt.eot?#iefix') format('embedded-opentype'),
-    url('./assets/font/fzylhjt.woff2') format('woff2'),
-    url('./assets/font/fzylhjt.woff') format('woff'),
-    /*火狐支持woff,woff2,opentype字体*/ url('./assets/font/fzylhjt.ttf')
-      format('truetype'),
-    /*基于webkit内核支持truetype字体*/
-      url('./assets/font/fzylhjt.svg#abandoned_bitplaneregular') format('svg');
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: ultra-expanded; /*定义字体符合拉伸*/
 }
 </style>
---
+++ src/components/arrow.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrap">
+    <img class="arrow__img"
+         src="@/assets/images/arrows.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'arrow'
+}
+</script>
+
+<style scoped lang='scss'>
+.wrap {
+  .arrow__img {
+    position: absolute;
+    width: 70px;
+    bottom: 50px;
+    left: 46%;
+    transform: translate(-50%, 0);
+    animation: start 1.8s infinite ease-in-out;
+    z-index: 993;
+  }
+  @keyframes start {
+    0%,
+    30% {
+      opacity: 0;
+      transform: translate(0, 10px);
+    }
+    60% {
+      opacity: 1;
+      transform: translate(0, 0);
+    }
+    100% {
+      opacity: 0;
+      transform: translate(0, -8px);
+    }
+  }
+}
+</style>
